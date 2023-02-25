fb8ef38e73507a3defa203c44f02401dddd83e1e
NicoleCB04 <126121300+NicoleCB04@users.noreply.github.com>
2023-02-24 23:11:25 -0500

    actualizacion de cadena de conexion para BD Nicole y Validaciones del CRUD Marcas

--- Peluches.BackOffice.Presentacion/Models/Marca.cs
+++ Peluches.BackOffice.Presentacion/Models/Marca.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,6 +14,9 @@ namespace Peluches.BackOffice.Presentacion.Models
         }
 
         public int IdMarca { get; set; }
+
+        [Required(ErrorMessage = "El campo 'Marca' es obligatorio.")]
+        [Display(Name = "Marca")]
         public string Marca1 { get; set; }
 
         public virtual ICollection<Producto> Productos { get; set; }
