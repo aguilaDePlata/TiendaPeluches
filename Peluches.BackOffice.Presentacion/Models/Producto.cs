732d852fbeb2dc507940cb9ed219b9e7d3a8af00
Ruth Liliana Carbajal Bejarano De Illescas <rulicb21@gmail.com>
2023-02-25 13:15:23 -0500

    Agrega proyecto ecomerce y validaciones de productos

--- Peluches.BackOffice.Presentacion/Models/Producto.cs
+++ Peluches.BackOffice.Presentacion/Models/Producto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Peluches.BackOffice.Presentacion.Validaciones;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -15,16 +16,17 @@ namespace Peluches.BackOffice.Presentacion.Models
 
         public int IdProducto { get; set; }
 
-        [Required(ErrorMessage = "El campo nombre de producto es obligatorio.")]
+        [Required(AllowEmptyStrings = true, ErrorMessage = "El campo nombre de producto es obligatorio.")]
         [Display(Name = "Nombre de Producto")]
         public string NombreProducto { get; set; }
 
-        [Required(ErrorMessage = "El campo descripción es obligatorio.")]
+        [Required(AllowEmptyStrings = true, ErrorMessage = "El campo descripción es obligatorio.")]
         [Display(Name = "Especificaciones Técnicas del Producto")]
         public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = "El campo precio de venta es obligatorio.")]
-        [Display (Name = "Precio de Venta")]
+        //[Required(ErrorMessage = "El campo precio de venta es obligatorio.")]
+        [Display(Name = "Precio de Venta")]
+        [ValidaPrecio]
         public decimal? PrecioVenta { get; set; }
 
         [Required(ErrorMessage = "Seleccione una marca.")]
@@ -39,10 +41,11 @@ namespace Peluches.BackOffice.Presentacion.Models
         [Display(Name = "Proveedor")]
         public int? IdProveedor { get; set; }
 
-        [Required(ErrorMessage = "Ingrese un stock mayor a cero.")]
+        [Required(ErrorMessage = "Ingrese cantidad de stock.")]
+        [Range(1, 2000, ErrorMessage = "Debe ingresar un valor entre 1 y 2000.")]
         public int? Stock { get; set; }
 
-        public bool? Activo { get; set; }
+        public bool Activo { get; set; }
 
         public virtual Marca Marca { get; set; }
         public virtual Modelo Modelo { get; set; }
