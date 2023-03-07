7006f94b90ce761ebf61ccd430d90921921bbf54
Ruth Liliana Carbajal Bejarano De Illescas <rulicb21@gmail.com>
2023-03-06 23:31:49 -0500

    feature: actualiza cadenas de conexion

--- Peluches.Web.API/Models/C3BdPedidosContext.cs
+++ Peluches.Web.API/Models/C3BdPedidosContext.cs
@@ -37,7 +37,7 @@ public partial class C3BdPedidosContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Initial Catalog=C3_BD_PEDIDOS;User Id=sa;Password=!sql2019;TrustServerCertificate=true;");
+        => optionsBuilder.UseSqlServer("Server=tcp:famiru-server-database.database.windows.net,1433;Initial Catalog=TiendaPeluchesDBAzure;Persist Security Info=False;User ID=userfamirusql;Password=Famiru123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
